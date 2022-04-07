9fb6c04af608decacc6ed7e09c8b37aec97f95b2
Rio Kato <rio@kato.com>
2022-04-07 18:11:25 +0900

    fixed

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 mkdir -p ~/.config
 ln -s ~/dotfiles/.config/nvim ~/.config/nvim
-mkdir -p ~/.vim
-ln -s ~/dotfiles/.vim/template ~/.vim/template
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.gdbinit ~/.gdbinit
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
