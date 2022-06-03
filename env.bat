f1daa9b98295767daea094d183eec3363ff1dee6
Rio Kato <rio@kato.com>
2022-06-03 21:26:42 +0900

    fixed

--- env.bat
+++
@@ -1,2 +0,0 @@
-setx _NT_SYMBOL_PATH "cache*c:\Symbols;srv*https://msdl.microsoft.com/download/symbols"
-mkdir c:\Symbols
--- setup.bat
+++
@@ -1,2 +0,0 @@
-rem mklink /D %HOMEPATH%\AppData\Local\nvim %HOMEPATH%\dotfiles\.config\nvim
-XCOPY /E /Y %HOMEPATH%\dotfiles\.config\nvim %HOMEPATH%\AppData\Local\nvim
