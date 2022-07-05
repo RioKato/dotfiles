8a15c2844b806b725eee05f913551744e7871e6c
Rio Kato <rio@kato.com>
2022-07-05 23:16:32 +0900

    updated

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -33,6 +33,8 @@ packer.startup(function()
 		requires = {
 			{ "hrsh7th/cmp-nvim-lsp" },
 			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "hrsh7th/cmp-cmdline" },
 			{ "hrsh7th/cmp-vsnip" },
 			{ "hrsh7th/vim-vsnip" },
 			{ "rafamadriz/friendly-snippets" },
@@ -150,7 +152,15 @@ cmp.setup({
 		{ name = "vsnip" },
 	}, {
 		{ name = "buffer" },
+		{ name = "path" },
 	}),
+})
+
+cmp.setup.cmdline("/", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
 })
 
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
