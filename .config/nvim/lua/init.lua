3f2ce4d6cfc7533cf8d03f6666237ea5be70480d
Rio Kato <rio@kato.com>
2022-07-09 03:52:38 +0900

    updated

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -156,13 +156,6 @@ cmp.setup({
 	}),
 })
 
-cmp.setup.cmdline("/", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = "buffer" },
-	},
-})
-
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
 local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require("lspconfig")
