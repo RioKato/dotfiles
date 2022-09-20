d6f47382e3956453f323fde5275b977343cf3961
rio <rio@kato.com>
2022-09-21 01:45:09 +0900

    fixed

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.gdbinit ~/.gdbinit
 ln -s ~/dotfiles/.docker/ ~/.docker 
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/.idapro/cfg ~/.idapro/cfg
 
