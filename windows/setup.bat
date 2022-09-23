8a4eca508cd9c63af373272bf831de2a9a33ef5d
rio <rio@kato.com>
2022-09-23 16:09:43 +0900

    fixed

--- windows/setup.bat
+++
@@ -1,8 +0,0 @@
-rem neovim
-mklink /D %HOMEPATH%\AppData\Local\nvim %HOMEPATH%\dotfiles\.config\nvim
-
-rem cdb
-setx _NT_SYMBOL_PATH "cache*c:\Symbols;srv*https://msdl.microsoft.com/download/symbols"
-mkdir c:\Symbols
-
-rem vcvarsall.bat
