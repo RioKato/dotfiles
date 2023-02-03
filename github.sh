2ea58b98495b5f0e5e82a07c60bb4e0fd2c67aa2
rio <rio@kato.com>
2023-02-04 06:30:25 +0900

    fixed

---
+++ github.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# python
+git clone https://gitlab.com/exploit-database/exploitdb.git
+git clone https://github.com/fortra/impacket.git
+git clone https://github.com/lgandx/Responder.git
+git clone https://github.com/ShawnDEvans/smbmap.git
+git clone https://github.com/Porchetta-Industries/CrackMapExec.git
+git clone --recursive https://github.com/BC-SECURITY/Empire.git
+git clone https://github.com/sqlmapproject/sqlmap.git
+git clone https://github.com/maurosoria/dirsearch.git 
+
+# ruby
+git clone https://github.com/Hackplayers/evil-winrm.git
+git clone https://github.com/digininja/CeWL.git
+
+# text
+git clone https://github.com/danielmiessler/SecLists.git
+git clone https://github.com/samratashok/nishang.git
+
+# binary
+# gobuster
+# ffuf
+# PEAS
+# pspy64
+# kerbrute
