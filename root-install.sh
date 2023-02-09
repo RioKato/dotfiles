23a4385287ba1ec70b7b79e5f3d6534b5afa5110
rio <rio@kato.com>
2023-02-10 01:44:16 +0900

    update

--- root-install.sh
+++ root-install.sh
@@ -116,3 +116,4 @@ snap install \
 ###############################################################################################################################################################
 
 git clone https://github.com/lgandx/Responder.git /opt/Responder.git
+git clone https://github.com/dirkjanm/krbrelayx.git /opt/krbrelayx.git
