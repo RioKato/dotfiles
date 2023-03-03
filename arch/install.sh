8fef0a6d0e930b9b3d2559bea2d544b396b5813e
rio <rio@kato.com>
2023-03-04 05:59:37 +0900

    update

--- arch/install.sh
+++ arch/install.sh
@@ -13,7 +13,6 @@ paru -S --noconfirm responder
 sudo systemctl start snapd
 sudo systemctl enable snapd
 sleep 10
-
 sudo snap install impacket crackmapexec metasploit-framework
 
 wget -P ~/Documents/petools https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh
