849a13554141a5b42112b40d9dd67588e8633289
RioKato <rio@kato.com>
2023-03-12 03:12:19 +0900

    update

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+for FILE in $(git ls-files)
+do
+  if [[ $FILE == .* ]] || [[ $FILE == bin/* ]]
+  then
+    SRC=`pwd`/$FILE
+    DST=~/$FILE
+    mkdir -p $(dirname $DST)
+    ln -snf $SRC $DST
+  fi
+done
+
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
