a9ae3be6871db8e91f2f955f23b578749ff15f96
rio <rio@kato.com>
2023-04-01 04:03:29 +0900

    update

--- arch/install.sh
+++ arch/install.sh
@@ -3,15 +3,7 @@
 cd $(dirname $0)
 sudo pacman -Syy
 sudo pacman -S --noconfirm - < pacman.txt
-
-paru -S --noconfirm snapd
 paru -S --noconfirm google-chrome
-paru -S --noconfirm nkf
-paru -S --noconfirm avaloniailspy
-
-sudo systemctl start snapd
-sleep 10
-sudo snap install crackmapexec metasploit-framework
 
 for URL in $(cat repo.txt)
 do
@@ -27,6 +19,7 @@ rustup default stable
 rustup component add rust-analyzer
 go install golang.org/x/tools/gopls@latest
 pip install python-lsp-server
+
 go install github.com/OJ/gobuster/v3@latest
 go install github.com/ffuf/ffuf@latest
 go install github.com/ropnop/kerbrute@latest
@@ -41,8 +34,6 @@ pip install bloodhound
 pip install pwntools
 pip install ROPGadget
 pip install git+https://github.com/GerbenJavado/LinkFinder.git
-pip install git+https://github.com/ShawnDEvans/smbmap.git
-pip install git+https://github.com/cytopia/smtp-user-enum.git
 gem install --user-install one_gadget
 npm install --prefix ~/Documents/js-beautify.npm js-beautify
 cargo install urlencode
