b488262db941552a5273be5905fa154a97933116
rio <rio@kato.com>
2023-04-01 21:29:57 +0900

    update

--- misc/kali/install.sh
+++ misc/kali/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+apt update
+apt install -y $(cat apt.txt)
+
 for URL in $(cat repo.txt)
 do
   git clone $URL ~/Documents/$(basename $URL)
@@ -9,3 +12,8 @@ for URL in $(cat tool.txt)
 do
   wget -P ~/Documents/tool $URL
 done
+
+go install github.com/ropnop/kerbrute@latest
+pip install git-dumper
+npm -g install js-beautify
+cargo install urlencode
