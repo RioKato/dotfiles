3793a30a2679bbf72d4c64a20deb9e9db2d286c7
rio <rio@kato.com>
2023-04-01 16:59:40 +0900

    update

--- arch/install.sh
+++ arch/install.sh
@@ -4,6 +4,7 @@ cd $(dirname $0)
 sudo pacman -Syy
 sudo pacman -S --noconfirm - < pacman.txt
 paru -S --noconfirm google-chrome
+chsh -s /bin/zsh
 
 rustup default stable
 rustup component add rust-analyzer
