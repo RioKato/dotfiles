37f66e722a031e739ffaaa48dda48bdbc234af9b
rio <rio@kato.com>
2023-04-01 17:26:11 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -16,3 +16,5 @@ done
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim --headless +PlugInstall +qa
+nvim --headless +PackerInstall +qa
