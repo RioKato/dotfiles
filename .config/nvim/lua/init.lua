aa6f506031be22bfa674cfbbba7f478dbb03e28d
rio <rio@kato.com>
2024-03-28 16:17:34 +0900

    update

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -120,6 +120,8 @@ vim.keymap.set("n", "<C-l>", "<cmd>QFToggle!<cr>")
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = "qf",
 	callback = function()
+		vim.keymap.set("n", "q", "<cmd>QFToggle!<cr>")
+		vim.keymap.set("n", "<enter>", "<cmd>QFToggle!<cr>")
 		vim.keymap.set("n", "dd", "<cmd>Reject<cr>")
 	end,
 })
