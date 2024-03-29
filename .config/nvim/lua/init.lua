d6f4d484378bf2ebcc1c211a83fb6b2d48085f36
rio <rio@kato.com>
2024-03-30 01:11:30 +0900

    update

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -48,7 +48,6 @@ packer.startup(function()
 		},
 	})
 	use("stevearc/qf_helper.nvim")
-	use("lewis6991/gitsigns.nvim")
 	use("tversteeg/registers.nvim")
 	use("ray-x/lsp_signature.nvim")
 end)
@@ -187,6 +186,3 @@ null_ls.setup({
 		null_ls.builtins.formatting.prettier,
 	},
 })
-
-local gitsigns = require("gitsigns")
-gitsigns.setup()
