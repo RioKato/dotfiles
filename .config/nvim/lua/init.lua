2aeb8fabad5a65d48cfeab8d0e36b785b3402bc8
rio <rio@kato.com>
2024-03-31 00:41:06 +0900

    update

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -40,7 +40,6 @@ packer.startup(function()
 		requires = {
 			{ "hrsh7th/cmp-nvim-lsp" },
 			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
 			{ "hrsh7th/cmp-cmdline" },
 			{ "hrsh7th/cmp-vsnip" },
 			{ "hrsh7th/vim-vsnip" },
