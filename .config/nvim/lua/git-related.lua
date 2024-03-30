01cc2f030d1fc4658bdacdf48ef8ee128d608720
rio <rio@kato.com>
2024-03-31 03:23:35 +0900

    update

--- .config/nvim/lua/git-related.lua
+++ .config/nvim/lua/git-related.lua
@@ -26,14 +26,14 @@ local function git_blame(path, from, to)
 	for i, v in ipairs(blame) do
 		local hash = string.match(v, "%S+")
 		if result[hash] == nil then
-			result[hash] = {{ from = i, to = i }}
-    else
-      lines = result[hash]
-      if lines[#lines].to + 1 == i then
-        lines[#lines].to = i
-      else
-        lines[#lines + 1] = { from = i, to = i }
-		end
+			result[hash] = { { from = i, to = i } }
+		else
+			lines = result[hash]
+			if lines[#lines].to + 1 == i then
+				lines[#lines].to = i
+			else
+				lines[#lines + 1] = { from = i, to = i }
+			end
 		end
 	end
 
@@ -86,33 +86,31 @@ M.git_related = function(path, from, to, opts)
 
 			if cache[path] and cache[path][hash] then
 				for _, pos in ipairs(cache[path][hash]) do
-					items[#items + 1] = { filename = path, lnum = pos.from , col = 1, text = hash}
+					items[#items + 1] = { filename = path, lnum = pos.from, col = 1, text = hash }
 				end
 			end
-
-      ::continue::
 		end
 	end
 
-  opts = opts or {}
-	pickers.new(opts, {
-    prompt_title = "Git Related",
-    finder = finders.new_table({
-      results = items,
-      entry_maker = opts.entry_maker or make_entry.gen_from_quickfix(opts),
-    }),
-    previewer = config.values.qflist_previewer(opts),
-    sorter = config.values.generic_sorter(opts),
-    push_cursor_on_edit = true,
-    push_tagstack_on_edit = true,
-  }):find()
+	opts = opts or {}
+	pickers
+		.new(opts, {
+			prompt_title = "Git Related",
+			finder = finders.new_table({
+				results = items,
+				entry_maker = opts.entry_maker or make_entry.gen_from_quickfix(opts),
+			}),
+			previewer = config.values.qflist_previewer(opts),
+			sorter = config.values.generic_sorter(opts),
+			push_cursor_on_edit = true,
+			push_tagstack_on_edit = true,
+		})
+		:find()
 end
 
 vim.cmd("sign define BlameSign linehl=DiffText")
 
 M.blame_highlight = function(path, line)
-  vim.cmd(string.format("sign unplace * group=BlameSign buffer=%d", vim.fn.bufnr()))
-
 	local command = { "git", "blame", "-l", "-s", "--", path }
 
 	local blame = vim.fn.systemlist(command)
@@ -122,27 +120,35 @@ M.blame_highlight = function(path, line)
 
 	for i, v in ipairs(blame) do
 		local hash = string.match(v, "%S+")
-    blame[i] = hash
-  end
+		blame[i] = hash
+	end
 
-  local hash = blame[line]
+	local hash = blame[line]
 	for i, v in ipairs(blame) do
-    if v == hash then
-      vim.cmd(string.format("sign place %d line=%d name=BlameSign group=BlameSign", i, i))
-    end
-  end
+		if v == hash then
+			vim.cmd(string.format("sign place %d line=%d name=BlameSign group=BlameSign", i, i))
+		end
+	end
+end
+
+M.blame_clear_highlight = function()
+	vim.cmd(string.format("sign unplace * group=BlameSign buffer=%d", vim.fn.bufnr()))
 end
 
 vim.api.nvim_create_user_command("GitRelated", function(opts)
-  if opts.range == 0 then
-    M.git_related(vim.fn.expand("%:p"))
-  else
-    M.git_related(vim.fn.expand("%:p"), opts.line1, opts.line2)
-  end
-end, {range = true})
+	if opts.range == 0 then
+		M.git_related(vim.fn.expand("%:p"))
+	else
+		M.git_related(vim.fn.expand("%:p"), opts.line1, opts.line2)
+	end
+end, { range = true })
 
 vim.api.nvim_create_user_command("BlameHighlight", function(opts)
-  M.blame_highlight(vim.fn.expand("%:p"), opts.line1)
-end, {range = true})
+	M.blame_highlight(vim.fn.expand("%:p"), opts.line1)
+end, { range = true })
+
+vim.api.nvim_create_user_command("BlameClearHighlight", function(opts)
+	M.blame_clear_highlight()
+end, {})
 
 return M
