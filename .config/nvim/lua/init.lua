6c8cc0fc256f32c56210c8d2e3ad20adf3dddd0a
rio <rio@kato.com>
2024-04-07 16:03:38 +0900

    update

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -103,6 +103,8 @@ require("lazy").setup({
 
 		config = function()
 			local null_ls = require("null-ls")
+			null_ls.builtins.formatting.prettier = { "markdown" }
+
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
