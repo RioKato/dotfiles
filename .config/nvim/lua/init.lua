99f4675d570ed30c8b4d5acd5c3d438324514145
rio <rio@kato.com>
2024-04-09 09:59:44 +0900

    update

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -189,8 +189,7 @@ require("lazy").setup({
 
 		config = function()
 			require("treesitter-context").setup({
-				max_lines = 1,
-				trim_scope = "inner",
+				multiline_threshold = 1,
 				separator = "━",
 			})
 
