de9cb5bf607760102426ce7383af09c031d72008
rio <rio@kato.com>
2025-05-18 04:36:12 +0900

    update

--- .config/nvim/ftplugin/c.lua
+++ .config/nvim/ftplugin/c.lua
@@ -1,8 +1,8 @@
 local tmux = require("tmux")
 
 vim.keymap.set("n", "r", function()
-    local break = string.format("break %s:%d", vim.fn.expand("%:p"), vim.fn.line("."))
-    local command = string.format("rr replay -- -ex %s -ex continue", vim.fn.shellescape( break))
+    local command = string.format("break %s:%d", vim.fn.expand("%:p"), vim.fn.line("."))
+    command = string.format("rr replay -- -ex %s -ex continue", vim.fn.shellescape(command))
     local directory = vim.fn.expand("%:p:h")
-    tmux.popup('rr', command, directory)
+    tmux.popup("rr", command, directory)
 end)
