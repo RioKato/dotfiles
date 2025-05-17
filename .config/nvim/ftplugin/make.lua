f15b0b127dd9f1af6ede2f04b096f9f9dee72cc4
rio <rio@kato.com>
2025-05-18 04:55:44 +0900

    update

--- .config/nvim/ftplugin/make.lua
+++ .config/nvim/ftplugin/make.lua
@@ -1,1 +1,7 @@
 vim.bo.expandtab = false
+local tmux = require("tmux")
+
+vim.keymap.set("n", "r", function()
+    local directory = vim.fn.expand("%:p:h")
+    tmux.popup("make", "make", directory)
+end)
