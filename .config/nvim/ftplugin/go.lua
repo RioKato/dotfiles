5967b40ed6c53bb9f4cfc3f52ab750a53c14df1a
Rio Kato <rio@kato.com>
2025-05-19 14:49:13 +0900

    update

--- .config/nvim/ftplugin/go.lua
+++ .config/nvim/ftplugin/go.lua
@@ -2,7 +2,7 @@ local tmux = require("tmux")
 
 vim.keymap.set("n", "r", function()
     local gomod = vim.fn.system({ "go", "env", "GOMOD" })
-    local script = gomod:gsub("go.mod\n$", "main.go")
+    local script = gomod:gsub("/go%.mod\n$", "/main.go")
     local command = string.format("go run %s", vim.fn.shellescape(script))
     tmux.popup("go", command)
 end, { buffer = true })
