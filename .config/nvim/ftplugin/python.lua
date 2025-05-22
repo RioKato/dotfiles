1851828266bb913c5a55a60a23aae2e0d0aee5d0
Rio Kato <rio@kato.com>
2025-05-22 20:19:17 +0900

    update

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -3,5 +3,11 @@ local tmux = require("tmux")
 vim.keymap.set("n", "r", function()
     local script = vim.fn.expand("%:p")
     local command = string.format("python3 %s", vim.fn.shellescape(script))
+    local venv = string.format("%s/bin/activate", vim.fn.getcwd())
+
+    if vim.fn.filereadable(venv) then
+        command = string.format("source %s && %s", vim.fn.shellescape(venv), command)
+    end
+
     tmux.popup("python", command)
 end, { buffer = true })
