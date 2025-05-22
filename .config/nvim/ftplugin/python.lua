1b4625a99d5de1327fa3bc9acedd636f812331d0
rio <rio@kato.com>
2025-05-22 21:50:45 +0900

    update

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", "r", function()
     local command = string.format("python3 %s", vim.fn.shellescape(script))
     local venv = string.format("%s/bin/activate", vim.fn.getcwd())
 
-    if vim.fn.filereadable(venv) then
+    if vim.fn.filereadable(venv) == 1 then
         command = string.format("source %s && %s", vim.fn.shellescape(venv), command)
     end
 
