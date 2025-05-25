562e1d42eaf874a6ff81abed42a773e5e64a6ef8
rio <rio@kato.com>
2025-05-25 22:43:38 +0900

    update

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -221,6 +221,10 @@ lazy().setup({
                         enabled = false,
                     },
                 },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 500,
+                },
             },
             sources = {
                 default = { "lsp", "path", "buffer" },
