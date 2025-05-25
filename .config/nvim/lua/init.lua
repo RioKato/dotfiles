c7d15c35ac9683a5389a75f295c809436b013b63
rio <rio@kato.com>
2025-05-25 13:02:53 +0900

    update

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -154,6 +154,24 @@ lazy().setup({
         end,
     },
 
+    {
+        "rapan931/lasterisk.nvim",
+
+        config = function()
+            local lasterisk = require("lasterisk")
+
+            vim.keymap.set("n", "*", function()
+                lasterisk.search()
+            end)
+            vim.keymap.set("n", "g*", function()
+                lasterisk.search({ is_whole = false })
+            end)
+            vim.keymap.set("x", "g*", function()
+                lasterisk.search({ is_whole = false })
+            end)
+        end,
+    },
+
     { "windwp/nvim-autopairs", opts = {} },
 
     { "machakann/vim-sandwich" },
