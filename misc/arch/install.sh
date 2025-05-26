1eedd1c7522ff01d4a9cf287a2111408f1e22f92
rio <rio@kato.com>
2025-05-26 22:11:57 +0900

    update

--- misc/arch/install.sh
+++ misc/arch/install.sh
@@ -3,13 +3,6 @@
 cd $(dirname $0)
 sudo pacman -Syy
 sudo pacman -S --noconfirm $(cat pacman.txt)
-paru -S --noconfirm google-chrome
 chsh -s /bin/zsh
-
-rustup default stable
-rustup component add rust-analyzer
-go install golang.org/x/tools/gopls@latest
-
 gsettings set org.gnome.desktop.interface gtk-key-theme "Emacs"
-
 exit 0
