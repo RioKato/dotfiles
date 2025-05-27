dea8b51746043c43e64b5f2303d91a8bae7b6eeb
rio <rio@kato.com>
2025-05-27 16:50:28 +0900

    fixed

--- misc/arch/install.sh
+++ misc/arch/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -ex
 
 cd $(dirname $0)
-sudo pacman -Syy
-sudo pacman -S --noconfirm $(cat pacman.txt)
+sudo -E pacman -Syy
+sudo -E pacman -S --noconfirm $(cat pacman.txt)
 chsh -s /bin/zsh
 gsettings set org.gnome.desktop.interface gtk-key-theme "Emacs"
 exit 0
