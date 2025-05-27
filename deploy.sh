678327412c75c73f406091e670583d2ab99d296d
rio <rio@kato.com>
2025-05-28 02:10:49 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,5 @@ done
 if command -v gdb >& /dev/null
 then
   wget -O ~/.gef.py -q https://gef.blah.cat/py
-  # wget -q -O- https://github.com/hugsy/gef/raw/main/scripts/gef-extras.sh | sh
   ln -snf ~/dotfiles/.gdbinit ~/.gdbinit
 fi
