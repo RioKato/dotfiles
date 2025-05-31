ccca3f4b97b510e2a2dadc99ecce46144dc477af
rio <rio@kato.com>
2025-05-31 22:37:08 +0900

    update

--- .config/nvim/lua/plugins/task.lua
+++ .config/nvim/lua/plugins/task.lua
@@ -2,25 +2,29 @@ return {
     {
         "is0n/jaq-nvim",
 
-        opts = {
-            cmds = {
-                external = {
-                    python = "[ -e bin/activate ] && source bin/activate; python3 %",
-                    rust = "cargo run",
-                    go = "go run $(dirname $(go env GOMOD))/main.go",
-                    javascript = "npm start",
-                    typescript = "npm start",
+        config = function()
+            require("jaq-nvim").setup({
+                cmds = {
+                    external = {
+                        python = "[ -e bin/activate ] && source bin/activate; python3 %",
+                        rust = "cargo run",
+                        go = "go run $(dirname $(go env GOMOD))/main.go",
+                        javascript = "npm start",
+                        typescript = "npm start",
+                    },
                 },
-            },
-            behavior = {
-                autosave = true,
-            },
-            ui = {
-                float = {
-                    height = 0.9,
-                    width = 0.9,
+                behavior = {
+                    autosave = true,
                 },
-            },
-        },
+                ui = {
+                    float = {
+                        height = 0.9,
+                        width = 0.9,
+                    },
+                },
+            })
+
+            vim.keymap.set("n", "r", "<cmd>Jaq<cr>")
+        end,
     },
 }
