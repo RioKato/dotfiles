b55f05bc78ddb7e84311926ff69453473c557e97
rio <rio@kato.com>
2025-06-02 22:04:14 +0900

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -54,19 +54,19 @@ local function init_editor()
 
     vim.keymap.set("n", "<esc><esc>", "<cmd>nohlsearch<cr>")
 
-    vim.api.nvim_create_user_command("Clean", function()
-        local winbufs = {}
+    vim.api.nvim_create_user_command("Clean", function(opts)
+        local winbufnrs = {}
 
         for _, winid in ipairs(vim.api.nvim_list_wins()) do
-            winbufs[vim.api.nvim_win_get_buf(winid)] = true
+            winbufnrs[vim.api.nvim_win_get_buf(winid)] = true
         end
 
         for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-            if not winbufs[bufnr] then
-                vim.api.nvim_buf_delete(bufnr, {})
+            if not winbufnrs[bufnr] then
+                pcall(vim.api.nvim_buf_delete, bufnr, { force = opts.bang })
             end
         end
-    end, {})
+    end, { bang = true })
 
     vim.api.nvim_create_autocmd("TextYankPost", {
         callback = function()
