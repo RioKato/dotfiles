dd7b718fab11e48f93a080196fac920a0caa3a35
rio <rio@kato.com>
2025-06-10 01:42:46 +0900

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -257,39 +257,6 @@ lazy().setup({
     },
 
     {
-        "t9md/vim-quickhl",
-
-        config = function()
-            vim.keymap.set({ "n", "x" }, "<C-t>", "<plug>(quickhl-manual-this)")
-            vim.keymap.set("n", "<leader>m", "<plug>(quickhl-manual-reset)")
-        end,
-    },
-
-    {
-        "unblevable/quick-scope",
-
-        init = function()
-            vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
-        end,
-    },
-
-    {
-        "rapan931/lasterisk.nvim",
-
-        config = function()
-            local lasterisk = require("lasterisk")
-
-            vim.keymap.set("n", "*", function()
-                lasterisk.search()
-            end)
-        end,
-    },
-
-    { "machakann/vim-sandwich" },
-
-    { "itchyny/vim-qfedit" },
-
-    {
         "nvim-treesitter/nvim-treesitter",
 
         opts = {
--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -1,5 +1,38 @@
 return {
     {
+        "t9md/vim-quickhl",
+
+        config = function()
+            vim.keymap.set({ "n", "x" }, "<C-t>", "<plug>(quickhl-manual-this)")
+            vim.keymap.set("n", "<leader>m", "<plug>(quickhl-manual-reset)")
+        end,
+    },
+
+    {
+        "unblevable/quick-scope",
+
+        init = function()
+            vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
+        end,
+    },
+
+    {
+        "rapan931/lasterisk.nvim",
+
+        config = function()
+            local lasterisk = require("lasterisk")
+
+            vim.keymap.set("n", "*", function()
+                lasterisk.search()
+            end)
+        end,
+    },
+
+    { "machakann/vim-sandwich" },
+
+    { "itchyny/vim-qfedit" },
+
+    {
         "windwp/nvim-autopairs",
 
         opts = {
