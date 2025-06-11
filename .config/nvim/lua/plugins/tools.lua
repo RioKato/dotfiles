133ba0defb450312e00952b8118cba2724d4f9cf
rio <rio@kato.com>
2025-06-11 11:27:26 +0900

    update

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -76,4 +76,16 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = {},
     },
+
+    {
+        "nvim-orgmode/orgmode",
+
+        opts = {
+            org_agenda_files = "~/orgfiles/**/*",
+            org_default_notes_file = "~/orgfiles/refile.org",
+            mappings = {
+                disable_all = true,
+            },
+        },
+    },
 }
