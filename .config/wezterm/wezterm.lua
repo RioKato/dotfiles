c317f1c31b4877d7d94c0df00035e07f7966805d
rio <rio@kato.com>
2025-06-27 02:49:57 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -15,6 +15,7 @@ config.keys = {
     { key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
     { key = "s", mods = "LEADER", action = wezterm.action.SplitVertical },
     { key = "v", mods = "LEADER", action = wezterm.action.SplitHorizontal },
+    { key = "z", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
     { key = "c", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
     { key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
     { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
