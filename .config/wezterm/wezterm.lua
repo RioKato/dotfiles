0dc70b7fed59ed9b1db2bb8abdf3b927483232db
rio <rio@kato.com>
2025-06-27 23:36:38 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,6 +6,7 @@ config.window_background_opacity = 0.9
 config.color_scheme = "Bamboo"
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.audible_bell = "Disabled"
+config.warn_about_missing_glyphs = false
 config.skip_close_confirmation_for_processes_named = {}
 
 config.disable_default_key_bindings = true
