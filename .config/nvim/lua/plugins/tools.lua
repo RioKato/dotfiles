bbb705fa81f12e452eeda7469e194701e9948a31
rio <rio@kato.com>
2025-10-04 04:42:33 +0900

    update

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -1,7 +1,6 @@
 return {
     {
         "stevearc/overseer.nvim",
-        dependencies = { "nvim-telescope/telescope.nvim" },
 
         config = function()
             require("overseer").setup({
