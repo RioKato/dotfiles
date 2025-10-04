c183d386c631ae195871488802e9132ca72384d2
rio <rio@kato.com>
2025-10-04 14:26:04 +0900

    update

--- .config/nvim/ftplugin/c.lua
+++
@@ -1,9 +0,0 @@
-local tmux = require("tmux")
-
--- vim.keymap.set("n", "r", function()
---     local breakpoint = string.format("break %s:%d", vim.fn.expand("%:p"), vim.fn.line("."))
---     local command = string.format("rr replay -- -ex %s -ex continue", vim.fn.shellescape(breakpoint))
---
---     vim.cmd("write")
---     tmux.popup("rr", command)
--- end, { buffer = true })
