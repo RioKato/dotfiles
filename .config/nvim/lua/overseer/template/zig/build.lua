e98142314535763774e2e56e555f728f92f8353c
rio <rio@kato.com>
2025-10-05 03:09:19 +0900

    update

--- .config/nvim/lua/overseer/template/zig/build.lua
+++ .config/nvim/lua/overseer/template/zig/build.lua
@@ -1,3 +1,7 @@
+local function get_build_zig(opts)
+    return vim.fs.find("build.zig", { upward = true, type = "file", path = opts.dir })[1]
+end
+
 return {
     name = "zig build",
 
@@ -9,6 +13,14 @@ return {
     end,
 
     condition = {
-        filetype = { "zig" },
+        callback = function(opts)
+            if vim.fn.executable("zig") == 0 then
+                return false, 'Command "zig" not found'
+            end
+            if not get_build_zig(opts) then
+                return false, "No build.zig found"
+            end
+            return true
+        end,
     },
 }
