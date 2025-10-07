875c35b327508671f72afbe5d799a2730043d9fa
rio <rio@kato.com>
2025-10-07 13:47:59 +0900

    update

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -13,16 +13,6 @@ return {
     {
         "iamcco/markdown-preview.nvim",
         build = ":call mkdp#util#install()",
-
-        config = function()
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = "markdown",
-                callback = function(ev)
-                    local opts = { buffer = ev.buf }
-                    vim.keymap.set("n", "r", "<cmd>MarkdownPreview<cr>", opts)
-                end,
-            })
-        end,
     },
 
     { "HakonHarnes/img-clip.nvim", opts = {} },
