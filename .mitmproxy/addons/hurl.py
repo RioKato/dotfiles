28b9a3f28dbee22e4b9224ce6768daeed59a9db5
rio <rio@kato.com>
2025-10-09 10:59:55 +0900

    update

--- .mitmproxy/addons/hurl.py
+++ .mitmproxy/addons/hurl.py
@@ -11,6 +11,67 @@ from mitmproxy import http
 from urllib.parse import urlparse, urlunparse, parse_qs
 
 
+def dumpreq(request: http.Request) -> str:
+    command = []
+    request = request.copy()
+    request.decode(strict=False)
+
+    request.headers.pop("content-length", None)
+
+    content_type = ""
+    if "content-type" in request.headers:
+        match request.headers.get("content-type", ""):
+            case "application/json":
+                content_type = "json"
+
+        if content_type:
+            request.headers.pop("content-type")
+
+    cookies = None
+    if "cookie" in request.headers:
+        cookies = SimpleCookie()
+        cookies.load(request.headers.get("cookie", ""))
+        request.headers.pop("cookie")
+
+    parsed = urlparse(request.url)
+    url = urlunparse(parsed._replace(query=""))
+    query = parse_qs(parsed.query, True)
+    command.append(f"{request.method} {url}")
+
+    for k, v in request.headers.items(multi=True):
+        command.append(f"{k}: {v}")
+
+    if query:
+        command.append("")
+        command.append("[Query]")
+
+        for k, v in query.items():
+            assert len(v) == 1
+            v = v[0]
+            command.append(f"{k}: {v}")
+
+    if cookies:
+        command.append("")
+        command.append("[Cookies]")
+
+        for k, v in cookies.items():
+            v = v.value
+            command.append(f"{k}: {v}")
+
+    if request.content:
+        content = request.content.decode()
+
+        if content.endswith("\n"):
+            content = content[:-1]
+
+        command.append("")
+        command.append(f"```{content_type}")
+        command.append(content)
+        command.append("```")
+
+    return "\n".join(command)
+
+
 class Hurl:
     @command.command("hurl")
     def save(self, flows: Sequence[flow.Flow], path: mitmproxy.types.Path) -> None:
@@ -18,68 +79,12 @@ class Hurl:
 
         for i, flow in enumerate(flows):
             assert isinstance(flow, http.HTTPFlow)
-            request = flow.request.copy()
-            request.decode(strict=False)
-
-            request.headers.pop("content-length", None)
-
-            content_type = ""
-            if "content-type" in request.headers:
-                match request.headers.get("content-type", ""):
-                    case "application/json":
-                        content_type = "json"
-
-                if content_type:
-                    request.headers.pop("content-type")
-
-            cookies = None
-            if "cookie" in request.headers:
-                cookies = SimpleCookie()
-                cookies.load(request.headers.get("cookie", ""))
-                request.headers.pop("cookie")
 
             command.append(f"#" * 15)
             command.append(f"# request {i:>3} #")
             command.append(f"#" * 15)
             command.append("")
-
-            parsed = urlparse(request.url)
-            url = urlunparse(parsed._replace(query=""))
-            query = parse_qs(parsed.query, True)
-            command.append(f"{request.method} {url}")
-
-            for k, v in request.headers.items(multi=True):
-                command.append(f"{k}: {v}")
-
-            if query:
-                command.append("")
-                command.append("[Query]")
-
-                for k, v in query.items():
-                    assert len(v) == 1
-                    v = v[0]
-                    command.append(f"{k}: {v}")
-
-            if cookies:
-                command.append("")
-                command.append("[Cookies]")
-
-                for k, v in cookies.items():
-                    v = v.value
-                    command.append(f"{k}: {v}")
-
-            if request.content:
-                content = request.content.decode()
-
-                if content.endswith("\n"):
-                    content = content[:-1]
-
-                command.append("")
-                command.append(f"```{content_type}")
-                command.append(content)
-                command.append("```")
-
-            command.append("")
+            command.append(dumpreq(flow.request))
 
         command = "\n".join(command)
 
