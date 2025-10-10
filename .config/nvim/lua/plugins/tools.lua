e3d34ecd6273276e40ab5b3632fdd49ec1235ac4
rio <rio@kato.com>
2025-10-11 05:01:41 +0900

    update

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -1,9 +1,12 @@
 return {
     {
         "stevearc/overseer.nvim",
+        dependencies = { "folke/snacks.nvim" },
 
         config = function()
-            require("overseer").setup({
+            local overseer = require("overseer")
+
+            overseer.setup({
                 task_list = {
                     direction = "left",
                 },
@@ -17,8 +20,36 @@ return {
                 },
             })
 
+            local function pick()
+                Snacks.picker.pick({
+                    title = "Tasks",
+                    layout = "select",
+
+                    finder = function()
+                        local tasks = overseer.list_tasks()
+                        local reversed = {}
+
+                        for i = #tasks, 1, -1 do
+                            tasks[i].text = tasks[i].name
+                            table.insert(reversed, tasks[i])
+                        end
+
+                        return reversed
+                    end,
+
+                    format = function(item)
+                        return { { string.format("%d. %s ", item.id, item.status) }, { item.name } }
+                    end,
+
+                    confirm = function(picker, item)
+                        picker:close()
+                        overseer.action_util.run_task_action(item)
+                    end,
+                })
+            end
+
             vim.keymap.set("n", "r", "<cmd>OverseerRun<cr>")
-            vim.keymap.set("n", "R", "<cmd>OverseerToggle<cr>")
+            vim.keymap.set("n", "R", pick)
         end,
     },
 
