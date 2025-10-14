9c821690fb50fc80975f5f12ef5af3f2ae1ef49a
rio <rio@kato.com>
2025-10-15 03:25:45 +0900

    update

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,30 +5,14 @@ return {
         config = function()
             local lss = {
                 "clangd",
+                "pyright",
+                "ts_ls",
+                "zls",
+                "stylua",
             }
 
             vim.lsp.enable(lss)
         end,
-    },
-
-    { "mason-org/mason.nvim", opts = {} },
-
-    {
-        "mason-org/mason-lspconfig.nvim",
-        dependencies = {
-            "mason-org/mason.nvim",
-            "neovim/nvim-lspconfig",
-        },
-
-        opts = {
-            ensure_installed = {
-                "pyright",
-                "ts_ls",
-                "zls",
-                "texlab",
-                "stylua",
-            },
-        },
     },
 
     {
@@ -49,19 +33,6 @@ return {
                 },
             })
         end,
-    },
-
-    {
-        "jay-babu/mason-null-ls.nvim",
-        dependencies = {
-            "mason-org/mason.nvim",
-            "nvimtools/none-ls.nvim",
-        },
-
-        opts = {
-            ensure_installed = nil,
-            automatic_installation = true,
-        },
     },
 
     {
