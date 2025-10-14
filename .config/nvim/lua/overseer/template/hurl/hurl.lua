e865bd9ddd8db7a82e566d7c5644342203bf7af3
rio <rio@kato.com>
2025-10-14 17:01:25 +0900

    update

--- .config/nvim/lua/overseer/template/hurl/hurl.lua
+++ .config/nvim/lua/overseer/template/hurl/hurl.lua
@@ -1,12 +1,13 @@
 return {
-    name = "hurl --path-as-is --very-verbose --no-output",
+    name = "hurl --path-as-is -A $UA --very-verbose --no-output",
 
     builder = function()
         local file = vim.fn.expand("%:p")
+        local ua = "Mozilla/5.0 (X11; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0"
 
         return {
             cmd = { "hurl" },
-            args = { "--path-as-is", "--very-verbose", "--no-output", file },
+            args = { "--path-as-is", "-A", ua, "--very-verbose", "--no-output", file },
         }
     end,
 
--- .config/nvim/lua/overseer/template/hurl/hurl_proxy.lua
+++ .config/nvim/lua/overseer/template/hurl/hurl_proxy.lua
@@ -1,12 +1,13 @@
 return {
-    name = "hurl --path-as-is --test -k -x http://localhost:8080",
+    name = "hurl --path-as-is -A $UA --test -k -x http://localhost:8080",
 
     builder = function()
         local file = vim.fn.expand("%:p")
+        local ua = "Mozilla/5.0 (X11; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0"
 
         return {
             cmd = { "hurl" },
-            args = { "--path-as-is", "--test", "-k", "-x", "http://localhost:8080", file },
+            args = { "--path-as-is", "-A", ua, "--test", "-k", "-x", "http://localhost:8080", file },
         }
     end,
 
