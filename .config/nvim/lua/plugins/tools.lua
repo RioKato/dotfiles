4028ff783a350486ffa1b9cd101831f1ea1d53e6
rio <rio@kato.com>
2025-10-16 01:52:19 +0900

    update

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -26,67 +26,61 @@ return {
         "stevearc/overseer.nvim",
         dependencies = { "folke/snacks.nvim" },
 
-        config = function()
-            local overseer = require("overseer")
+        opts = {
+            task_list = {
+                direction = "left",
+            },
+            templates = {
+                "builtin",
+                "python",
+                "zig",
+                "latex",
+                "hurl",
+            },
+        },
 
-            overseer.setup({
-                task_list = {
-                    direction = "left",
-                },
+        keys = {
+            { "r", "<cmd>OverseerRun<cr>" },
+            {
+                "R",
+                function()
+                    local overseer = require("overseer")
 
-                templates = {
-                    "builtin",
-                    "python",
-                    "zig",
-                    "latex",
-                    "hurl",
-                },
-            })
+                    Snacks.picker.pick({
+                        title = "Tasks",
+                        layout = "select",
 
-            local function pick()
-                Snacks.picker.pick({
-                    title = "Tasks",
-                    layout = "select",
+                        finder = function()
+                            local tasks = overseer.list_tasks()
+                            local reversed = {}
 
-                    finder = function()
-                        local tasks = overseer.list_tasks()
-                        local reversed = {}
+                            for i = #tasks, 1, -1 do
+                                tasks[i].text = string.format("%s %s", tasks[i].status, tasks[i].name)
+                                table.insert(reversed, tasks[i])
+                            end
 
-                        for i = #tasks, 1, -1 do
-                            tasks[i].text = string.format("%s %s", tasks[i].status, tasks[i].name)
-                            table.insert(reversed, tasks[i])
-                        end
+                            return reversed
+                        end,
 
-                        return reversed
-                    end,
+                        format = function(item)
+                            return {
+                                { string.format("%d. ", item.id) },
+                                { item.text, string.format("Overseer%s", item.status) },
+                            }
+                        end,
 
-                    format = function(item)
-                        return {
-                            { string.format("%d. ", item.id) },
-                            { item.text, string.format("Overseer%s", item.status) },
-                        }
-                    end,
+                        confirm = function(picker, item)
+                            if item == nil then
+                                return
+                            end
 
-                    confirm = function(picker, item)
-                        if item == nil then
-                            return
-                        end
-
-                        picker:close()
-                        overseer.run_action(item, "open float")
-                    end,
-                })
-            end
-
-            local keys = {
-                { "n", "r", "<cmd>OverseerRun<cr>" },
-                { "n", "R", pick },
-            }
-
-            for _, key in ipairs(keys) do
-                vim.keymap.set(key[1], key[2], key[3])
-            end
-        end,
+                            picker:close()
+                            overseer.run_action(item, "open float")
+                        end,
+                    })
+                end,
+            },
+        },
     },
 
     {
