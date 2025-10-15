7f2a06d28f703ab853bfeedea46d305c2aaeb8bb
rio <rio@kato.com>
2025-10-15 15:11:09 +0900

    update

---
+++ .mitmproxy/addons/bopen.py
@@ -0,0 +1,19 @@
+from mitmproxy import command
+from mitmproxy import flow
+from tempfile import mkstemp
+import webbrowser
+
+
+class BOpen:
+    @command.command("bopen")
+    def open(self, flow: flow.Flow):
+        response = flow.response.copy()
+        response.decode(strict=False)
+        temp = mkstemp()
+
+        with open(temp[1], "w+b") as fd:
+            fd.write(response.content)
+            webbrowser.open(temp[1])
+
+
+addons = [BOpen()]
