7adc8a356016760da1a5612b02e7bba25d9013a0
rio <rio@kato.com>
2025-10-28 10:57:43 +0900

    update

---
+++ .config/nvim/lua/plugins/local/tmux.lua
@@ -0,0 +1,68 @@
+local Tmux = {}
+
+function Tmux.exec(cmd)
+    vim.fn.system({ "tmux", unpack(cmd) })
+end
+
+function Tmux:wincmd(hjkl)
+    local map = {
+        h = "-L",
+        j = "-D",
+        k = "-U",
+        l = "-R",
+    }
+
+    self.exec({ "selectp", map[hjkl] })
+end
+
+local Navi = {}
+
+function Navi.wincmd(hjkl)
+    local prev = vim.api.nvim_get_current_win()
+    vim.cmd.wincmd(hjkl)
+
+    if prev == vim.api.nvim_get_current_win() then
+        Tmux:wincmd(hjkl)
+    end
+end
+
+local M = {}
+
+function M.setup()
+    local keys = {
+        {
+            "n",
+            "<C-w>h",
+            function()
+                Navi.wincmd("h")
+            end,
+        },
+        {
+            "n",
+            "<C-w>j",
+            function()
+                Navi.wincmd("j")
+            end,
+        },
+        {
+            "n",
+            "<C-w>k",
+            function()
+                Navi.wincmd("k")
+            end,
+        },
+        {
+            "n",
+            "<C-w>l",
+            function()
+                Navi.wincmd("l")
+            end,
+        },
+    }
+
+    for _, key in ipairs(keys) do
+        vim.keymap.set(key[1], key[2], key[3])
+    end
+end
+
+return M
