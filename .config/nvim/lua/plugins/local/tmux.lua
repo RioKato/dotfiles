e76a14d4e2154ca3627d4e7c106357b298e36305
rio <rio@kato.com>
2025-10-28 14:41:08 +0900

    update

--- .config/nvim/lua/plugins/local/tmux.lua
+++ .config/nvim/lua/plugins/local/tmux.lua
@@ -4,19 +4,20 @@ function Tmux.exec(cmd)
     vim.fn.system({ "tmux", unpack(cmd) })
 end
 
-function Tmux:wincmd(hjkl)
-    local map = {
-        h = "-L",
-        j = "-D",
-        k = "-U",
-        l = "-R",
-    }
-
-    self.exec({ "selectp", map[hjkl] })
+function Tmux:enabled()
+    return vim.env.TMUX ~= nil
 end
 
-function Tmux:zoom()
-    self.exec({ "resizep", "-Z" })
+function Tmux:wincmd(cmd)
+    local map = {
+        h = { "selectp", "-L" },
+        j = { "selectp", "-D" },
+        k = { "selectp", "-U" },
+        l = { "selectp", "-R" },
+        z = { "resizep", "-Z" },
+    }
+
+    self.exec(map[cmd])
 end
 
 local Navi = {}
@@ -30,21 +31,31 @@ function Navi:new(zen)
     return obj
 end
 
-function Navi:wincmd(hjkl)
-    local prev = vim.api.nvim_get_current_win()
-    vim.cmd.wincmd(hjkl)
+function Navi:wincmd(cmd)
+    if vim.tbl_contains({ "h", "j", "k", "l" }, cmd) then
+        local prev = vim.api.nvim_get_current_win()
+        vim.cmd.wincmd(cmd)
 
-    if vim.env.TMUX and prev == vim.api.nvim_get_current_win() then
-        Tmux:wincmd(hjkl)
+        if prev == vim.api.nvim_get_current_win() then
+            if Tmux:enabled() then
+                Tmux:wincmd(cmd)
+            end
+        end
+
+        return
     end
-end
 
-function Navi:zoom()
-    if self.zen then
-        self.zen()
+    if cmd == "z" then
+        if self.zen then
+            self.zen()
+        end
+
+        if Tmux:enabled() then
+            Tmux:wincmd(cmd)
+        end
+
+        return
     end
-
-    Tmux:zoom()
 end
 
 local M = {}
@@ -53,46 +64,10 @@ function M.setup(opts)
     opts = opts or {}
     local navi = Navi:new(opts.zen)
 
-    local keys = {
-        {
-            "n",
-            "<C-w>h",
-            function()
-                navi:wincmd("h")
-            end,
-        },
-        {
-            "n",
-            "<C-w>j",
-            function()
-                navi:wincmd("j")
-            end,
-        },
-        {
-            "n",
-            "<C-w>k",
-            function()
-                navi:wincmd("k")
-            end,
-        },
-        {
-            "n",
-            "<C-w>l",
-            function()
-                navi:wincmd("l")
-            end,
-        },
-        {
-            "n",
-            "<C-w>z",
-            function()
-                navi:zoom()
-            end,
-        },
-    }
-
-    for _, key in ipairs(keys) do
-        vim.keymap.set(key[1], key[2], key[3])
+    for _, cmd in ipairs({ "h", "j", "k", "l", "z" }) do
+        vim.keymap.set("n", string.format("<C-w>%s", cmd), function()
+            navi:wincmd(cmd)
+        end)
     end
 end
 
