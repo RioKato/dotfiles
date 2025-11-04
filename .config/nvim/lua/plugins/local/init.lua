8edc2623f09ad3e07bcfb6cec0c741a5601d4139
rio <rio@kato.com>
2025-11-04 17:24:56 +0900

    update

--- .config/nvim/lua/plugins/local/init.lua
+++ .config/nvim/lua/plugins/local/init.lua
@@ -6,25 +6,6 @@ function init.hover()
     require(root .. ".hover").setup()
 end
 
-function init.tmux()
-    local tmux = require(root .. ".tmux")
-
-    vim.keymap.set("n", "<C-w>z", function()
-        local opts = {
-            toggles = {
-                dim = false,
-            },
-        }
-
-        if tmux.active() then
-            opts.on_open = tmux.zoom.on
-            opts.on_close = tmux.zoom.off
-        end
-
-        Snacks.zen(opts)
-    end)
-end
-
 function init.wezterm()
     local wezterm = require(root .. ".wezterm")
     wezterm.setup("<C-q>")
@@ -32,7 +13,6 @@ end
 
 function init:all()
     self.hover()
-    -- self.tmux()
     self.wezterm()
 end
 
