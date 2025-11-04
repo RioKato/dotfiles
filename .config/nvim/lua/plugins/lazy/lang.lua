5e8f5e49ae06fbcb5dbad6d47635ce24c6a52b90
rio <rio@kato.com>
2025-11-04 15:56:30 +0900

    update

--- .config/nvim/lua/plugins/lazy/lang.lua
+++ .config/nvim/lua/plugins/lazy/lang.lua
@@ -70,4 +70,13 @@ return {
             },
         },
     },
+
+    {
+        "voldikss/vim-translator",
+
+        config = function()
+            vim.g.translator_target_lang = "ja"
+            vim.g.translator_default_engines = { "google" }
+        end,
+    },
 }
