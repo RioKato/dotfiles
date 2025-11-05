4067f001a950f6fbe7329deffb66513fc7a3f84a
rio <rio@kato.com>
2025-11-05 11:04:38 +0900

    update

--- .config/nvim/lua/plugins/lazy/debug.lua
+++ .config/nvim/lua/plugins/lazy/debug.lua
@@ -52,7 +52,11 @@ return {
 
     {
         "rcarriga/nvim-dap-ui",
-        dependencies = { "mfussenegger/nvim-dap" },
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
+        },
+        opts = {},
     },
 
     {
