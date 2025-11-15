0b6313a0dfa6dbe5769dc589cf2af009fa561d74
rio <rio@kato.com>
2025-11-16 00:37:37 +0900

    update

--- .config/nvim/lua/plugins/local/init.lua
+++ .config/nvim/lua/plugins/local/init.lua
@@ -3,16 +3,16 @@ local root = "plugins.local"
 local init = {}
 
 function init.hover()
-    require(root .. ".hover").setup()
+	require(root .. ".hover").setup()
 end
 
 function init.wezterm()
-    require(root .. ".wezterm").setup()
+	require(root .. ".wezterm").setup()
 end
 
 function init:all()
-    self.hover()
-    self.wezterm()
+	self.hover()
+	self.wezterm()
 end
 
 init:all()
